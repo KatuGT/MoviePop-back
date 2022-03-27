2fbe8690e41f71287274f6fe03b685dc7ed129bb
JimenaKatuMartinez <93katu@gmail.com>
2022-03-27 10:40:09 -0300

    Agregado cors()

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const autenticacionRuta = require("./rutas/AutenticacionRutas")
 const usuarioRuta = require("./rutas/UsuarioRutas")
+const cors = require("cors");
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL, () => {
     console.log(`conectado a la base de datos: ${process.env.MONGO_URL}`)
 })
 
+
+app.use(cors())
 app.use(express.json())
 app.use("/api/aut", autenticacionRuta)
 app.use("/api/usuario", usuarioRuta)
