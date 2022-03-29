f61224f1fccbc78cb6ff78b7fdbc77c0a3a419cb
Cintia Martinez <93katu@gmail.com>
2022-03-29 13:52:30 -0300

    edicion en verificacion

--- verificacion.js
+++ verificacion.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 function verificacion(req, res, next){
     const autHeader = req.headers.token;
     if(autHeader){
-        const token = autHeader.split(" ")[1]
-        jwt.verify(token, process.env.SECRET_KEY, (error, usuario) =>{
+        
+        jwt.verify(autHeader, process.env.SECRET_KEY, (error, usuario) =>{
             if (error) res.status(403).json("El token no es valido");
             req.usuario = usuario
             next()
